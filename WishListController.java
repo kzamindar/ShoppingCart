9e7e7912de02b1ea374f62caa2022d11bc70f2fb
Kshitiz Zamindar <67591580+kzamindar@users.noreply.github.com>
2022-03-29 22:08:23 +0530

    Add files via upload

---
+++ WishListController.java
@@ -0,0 +1,54 @@
+package com.shoppingcart.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shoppingcart.entity.Product;
+import com.shoppingcart.entity.User;
+import com.shoppingcart.entity.WishList;
+
+@RestController
+@RequestMapping("/wishlist")
+public class WishListController {
+
+        @Autowired
+        private WishListService wishListService;
+
+        @Autowired
+        private AuthenticationService authenticationService;
+
+        @GetMapping("/{token}")
+        public ResponseEntity<List<ProductDto>> getWishList(@PathVariable("token") String token) {
+                int user_id = authenticationService.getUser(token).getId();
+                List<WishList> body = wishListService.readWishList(user_id);
+                List<ProductDto> products = new ArrayList<ProductDto>();
+                for (WishList wishList : body) {
+                        products.add(ProductService.getDtoFromProduct(wishList.getProduct()));
+                }
+
+                return new ResponseEntity<List<ProductDto>>(products, HttpStatus.OK);
+        }
+
+        @PostMapping("/add")
+        public ResponseEntity<ApiResponse> addWishList(@RequestBody Product product, @RequestParam("token") String token) {
+                authenticationService.authenticate(token);
+                User user = authenticationService.getUser(token);
+                WishList wishList = new WishList(user, product);
+                wishListService.createWishlist(wishList);
+                return new ResponseEntity<ApiResponse>(new ApiResponse(true, "Add to wishlist"), HttpStatus.CREATED);
+
+        }
+
+
+}
